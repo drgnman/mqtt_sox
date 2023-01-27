5e40cecc120f84c5db5848c1f57cb560ff23c9f1
drgnman <drgnman@ht.sfc.keio.ac.jp>
2023-01-28 07:24:44 +0900

    createの処理部分を削除

--- publisher.py
+++ publisher.py
@@ -5,30 +5,6 @@ client = connection.connect()
 
 publisher = PublishModule(client)
 node = Node("test_node")
-node.setLocation(80.5, 123.4)
-transducer = node.Transducer("test_transducer1")
-transducer.setUnit("%")
-transducer.setMinValue(0)
-transducer.setMaxValue(100)
-transducer.setDescription("hogehoge")
-node.appendTransducer(transducer)
-
-transducer = node.Transducer("test_transducer2")
-transducer.setUnit("percentage")
-transducer.setMinValue(1.2)
-transducer.setMaxValue(99.9)
-transducer.setDescription("fugafuga")
-node.appendTransducer(transducer)
-
-transducer = node.Transducer("test_transducer3")
-transducer.setUnit("C")
-transducer.setMinValue(-20)
-transducer.setMaxValue(40)
-transducer.setDescription("nugaaa")
-node.appendTransducer(transducer)
-
-publisher.create(node)
-
 node.flushTransducers()
 
 transducer = node.Transducer("test_transducer1")
