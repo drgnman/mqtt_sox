032f51212dc4d512d16aaacfb87bb52ec9979815
drgnman <drgnman@sfc.keio.ac.jp>
2023-01-27 17:26:10 +0900

    modify

--- subscriber.py
+++ subscriber.py
@@ -1,8 +1,8 @@
 from mqtt_sox import Connection, SubscribeModule
 
-connection = Connection("emqx.co.mqtt", 1883, "test")
-# client = connection.connect()
-subscriber = SubscribeModule("client")
+connection = Connection("localhost", 1883, "test")
+client = connection.connect()
+subscriber = SubscribeModule(client)
 node_name = "test_node"
 subscriber.subscribe(node_name)
 subscriber.run()
