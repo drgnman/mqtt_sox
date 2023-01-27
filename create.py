b27697a11f4dc9eb70ff5a4c070b181ca3cb973e
drgnman <drgnman@ht.sfc.keio.ac.jp>
2023-01-28 07:24:27 +0900

    createとpublishのsampleプログラムを分割

---
+++ create.py
@@ -0,0 +1,31 @@
+
+from mqtt_sox import Connection, PublishModule, Node
+
+connection = Connection("localhost", 1883, "publish_test")
+# client = connection.connect()
+
+publisher = PublishModule("test")
+node = Node("test_node")
+node.setLocation(80.5, 123.4)
+transducer = node.Transducer("test_transducer1")
+transducer.setUnit("%")
+transducer.setMinValue(0)
+transducer.setMaxValue(100)
+transducer.setDescription("hogehoge")
+node.appendTransducer(transducer)
+
+transducer = node.Transducer("test_transducer2")
+transducer.setUnit("percentage")
+transducer.setMinValue(1.2)
+transducer.setMaxValue(99.9)
+transducer.setDescription("fugafuga")
+node.appendTransducer(transducer)
+
+transducer = node.Transducer("test_transducer3")
+transducer.setUnit("C")
+transducer.setMinValue(-20)
+transducer.setMaxValue(40)
+transducer.setDescription("nugaaa")
+node.appendTransducer(transducer)
+
+publisher.create(node)
