70117bb734a233c4d4127a6f70bdf7bdf3295ac2
drgnman <drgnman@sfc.keio.ac.jp>
2023-01-27 18:29:40 +0900

    データ受信時の処理記載のoverrideに対応させた

--- subscriber.py
+++ subscriber.py
@@ -1,8 +1,25 @@
 from mqtt_sox import Connection, SubscribeModule
 
+# サブスクライブした時の処理をOverrideする方法
+class OriginalSubscribeModule(SubscribeModule):
+    def __init__(self, client):
+        super().__init__(client)
+
+    def setProcessOnMessage(self):
+        def on_message(client, userdata, msg):
+            print(f"hogeeeee!!!! Received {msg.payload} from {msg.topic} topic")
+        self.client.on_message = on_message
+
 connection = Connection("localhost", 1883, "subscribe_test")
 client = connection.connect()
-subscriber = SubscribeModule(client)
-node_name = "test_node"
+node_name = "test_node2"
+
+# 単純にreceiveするだけの時はこっちでよし
+# subscriber = SubscribeModule(client)
+# subscriber.subscribe(node_name)
+# subscriber.setProcessOnMessage()
+subscriber = OriginalSubscribeModule(client)
 subscriber.subscribe(node_name)
+subscriber.setProcessOnMessage()
+
 subscriber.run()
